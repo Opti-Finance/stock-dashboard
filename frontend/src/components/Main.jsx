1145277db5a2429d7f026dc2d2b4116da9869ac5
Tian Yao <xtyao2015@gmail.com>
2024-07-15 23:34:44 -0400

    changed font and title name

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -23,7 +23,7 @@ const Main = () => {
 
     return (
         <div className='flex flex-col items-center justify-center min-h-screen text-center w-full' id="home">
-            <p className="text-7xl my-5 dark:text-white">GOATED Financial Advisor</p>
+            <p className="text-7xl my-5 dark:text-white">OmniFinance Solutions</p>
             <a 
                 key="see-models-link" 
                 href="#models" 
