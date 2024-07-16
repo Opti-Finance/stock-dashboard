b23e4929a915e5e48dc354bf13f8e6fa2e712572
Tian Yao <xtyao2015@gmail.com>
2024-07-15 23:23:06 -0400

    added corresponding dark text

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,8 +1,8 @@
 const About = () => {
     return (
         <>
-            <h1 id="about" className="w-full text-5xl flex justify-center">About Us</h1>
-            <p className="w-full text-xl flex justify-center mt-5 mb-20">We are a goated team that builds goated products yesssir</p>
+            <h1 id="about" className="w-full dark:text-white text-5xl flex justify-center">About Us</h1>
+            <p className="w-full dark:text-white text-xl flex justify-center mt-5 mb-20">We are a goated team that builds goated products yesssir</p>
         </>
     )
 }
--- frontend/src/components/Models.jsx
+++ frontend/src/components/Models.jsx
@@ -3,10 +3,10 @@ import botData from "../lib/bot-data"
 
 const Models = () => {
     return (
-        <div id="models" className="flex-grow">
-            <h1 className="flex justify-center text-5xl">Models</h1>
+        <>
+            <h1 id="models" className="flex justify-center text-5xl dark:text-white">Models</h1>
             <Carousel data={botData} /> 
-        </div>
+        </>
     )
 }
 
